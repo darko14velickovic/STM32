34c2a6622cf42d6f2257eb409f5372c725c36a0b
Darko Velickovic <darko.velickovic14@gmail.com>
2017-10-02 22:21:19 +0200

    added base64 encoding and decoding

--- source/rtos/freertos.c
+++ source/rtos/freertos.c
@@ -108,6 +108,7 @@ extern uint8_t NumberPressed;
 //Task variables
 
 uint32_t debounceCounter = 0x1FFFF;
+uint32_t rng = 0xFFFFFFFF;
 
 uint8_t characterCount = 28;
 
@@ -118,17 +119,181 @@ char avaibleCharacters[28]  = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
 uint8_t populateCounter = 0;															 
 char shownBuffer[32] = {0};
 
-char encryptedBuffer[32] = {0};
+char encryptedBuffer[64] = {0};
 
-char OTP = 0x23;
+char OTP = 0xAA;
 
 bool ValueSelected = 0;
+
+bool Encrypt = 1;
+
+static const unsigned char base64_enc_map[64] =
+{
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
+    'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+    'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
+    '8', '9', '+', '/'
+};
+
+static const unsigned char base64_dec_map[128] =
+{
+    127, 127, 127, 127, 127, 127, 127, 127, 127, 127,
+    127, 127, 127, 127, 127, 127, 127, 127, 127, 127,
+    127, 127, 127, 127, 127, 127, 127, 127, 127, 127,
+    127, 127, 127, 127, 127, 127, 127, 127, 127, 127,
+    127, 127, 127,  62, 127, 127, 127,  63,  52,  53,
+     54,  55,  56,  57,  58,  59,  60,  61, 127, 127,
+    127,  64, 127, 127, 127,   0,   1,   2,   3,   4,
+      5,   6,   7,   8,   9,  10,  11,  12,  13,  14,
+     15,  16,  17,  18,  19,  20,  21,  22,  23,  24,
+     25, 127, 127, 127, 127, 127, 127,  26,  27,  28,
+     29,  30,  31,  32,  33,  34,  35,  36,  37,  38,
+     39,  40,  41,  42,  43,  44,  45,  46,  47,  48,
+     49,  50,  51, 127, 127, 127, 127, 127
+};
+
+/*
+ * Decode a base64-formatted buffer
+ */
+int base64_decode( unsigned char *dst, size_t *dlen,
+                   const unsigned char *src, size_t slen )
+{
+    size_t i, n;
+    uint32_t j, x;
+    unsigned char *p;
+
+    for( i = j = n = 0; i < slen; i++ )
+    {
+        if( ( slen - i ) >= 2 &&
+            src[i] == '\r' && src[i + 1] == '\n' )
+            continue;
+
+        if( src[i] == '\n' )
+            continue;
+
+        if( src[i] == '=' && ++j > 2 )
+            return( -2 );
+
+        if( src[i] > 127 || base64_dec_map[src[i]] == 127 )
+            return( -2);
+
+        if( base64_dec_map[src[i]] < 64 && j != 0 )
+            return( -2 );
+
+        n++;
+    }
+
+    if( n == 0 )
+        return( 0 );
+
+    n = ((n * 6) + 7) >> 3;
+
+    if( *dlen < n )
+    {
+        *dlen = n;
+        return( -3 );
+    }
+
+   for( j = 3, n = x = 0, p = dst; i > 0; i--, src++ )
+   {
+        if( *src == '\r' || *src == '\n' )
+            continue;
+
+        j -= ( base64_dec_map[*src] == 64 );
+        x  = (x << 6) | ( base64_dec_map[*src] & 0x3F );
+
+        if( ++n == 4 )
+        {
+            n = 0;
+            if( j > 0 ) *p++ = (unsigned char)( x >> 16 );
+            if( j > 1 ) *p++ = (unsigned char)( x >>  8 );
+            if( j > 2 ) *p++ = (unsigned char)( x       );
+        }
+    }
+
+    *dlen = p - dst;
+
+    return( 0 );
+}
+
+
+
+/*
+ * Encode a buffer into base64 format
+ */
+int base64_encode( unsigned char *dst, size_t *dlen,
+                   const unsigned char *src, size_t slen )
+{
+    size_t i, n;
+    int C1, C2, C3;
+    unsigned char *p;
+
+    if( slen == 0 )
+        return( 0 );
+
+    n = (slen << 3) / 6;
+
+    switch( (slen << 3) - (n * 6) )
+    {
+        case  2: n += 3; break;
+        case  4: n += 2; break;
+        default: break;
+    }
+
+    if( *dlen < n + 1 )
+    {
+        *dlen = n + 1;
+        return( -1 );
+    }
+
+    n = (slen / 3) * 3;
+
+    for( i = 0, p = dst; i < n; i += 3 )
+    {
+        C1 = *src++;
+        C2 = *src++;
+        C3 = *src++;
+
+        *p++ = base64_enc_map[(C1 >> 2) & 0x3F];
+        *p++ = base64_enc_map[(((C1 &  3) << 4) + (C2 >> 4)) & 0x3F];
+        *p++ = base64_enc_map[(((C2 & 15) << 2) + (C3 >> 6)) & 0x3F];
+        *p++ = base64_enc_map[C3 & 0x3F];
+    }
+
+    if( i < slen )
+    {
+        C1 = *src++;
+        C2 = ((i + 1) < slen) ? *src++ : 0;
+
+        *p++ = base64_enc_map[(C1 >> 2) & 0x3F];
+        *p++ = base64_enc_map[(((C1 & 3) << 4) + (C2 >> 4)) & 0x3F];
+
+        if( (i + 1) < slen )
+             *p++ = base64_enc_map[((C2 & 15) << 2) & 0x3F];
+        else *p++ = '=';
+
+        *p++ = '=';
+    }
+
+    *dlen = p - dst;
+    *p = 0;
+
+    return( 0 );
+}
 
 void UserInteractionTask(void const * argument)
 {
 	
 	for(;;)
   {
+		if((GPIOReadPin(BUTTON_PORT,BUTTON_PIN) == GPIO_PIN_SET) && ButtonPressed)
+		{
+			rng--;
+		}
+		
 		if((GPIOReadPin(BUTTON_PORT,BUTTON_PIN) == GPIO_PIN_SET) && ButtonPressed && !ValueSelected)
 		{
 			debounceCounter--;
@@ -167,11 +332,27 @@ void UserInteractionTask(void const * argument)
 					if(NumberPressed == 27)
 					{
 						//hash and encryption
-						
-						for(uint8_t i = 0 ; i < 32; i++)
+						if(Encrypt == 1)
 						{
-							encryptedBuffer[i] = shownBuffer[i] ^ OTP ^ debounceCounter;
+							for(uint8_t i = 0 ; i < 32; i++)
+							{
+								if(shownBuffer[i] == ' ')
+								{
+									shownBuffer[i] = 0;
+								}
+								
+								encryptedBuffer[i] = shownBuffer[i] ^ OTP ^ rng;
+							}
+							size_t len = sizeof(encryptedBuffer);
+							
+							base64_encode( encryptedBuffer, &len, shownBuffer, 32 );
 						}
+						else
+						{
+							size_t len = sizeof(encryptedBuffer);
+							base64_decode( encryptedBuffer, &len, shownBuffer, 64 );
+						}
+						
 						
 						//show result
 						xTaskNotifyGive(defaultTaskHandle);
@@ -187,14 +368,8 @@ void UserInteractionTask(void const * argument)
 					}
 						
 				}
-				
-				
-				
-				
-				
 			}
-			
-			
+
 		}
 		else if((GPIOReadPin(BUTTON_PORT,BUTTON_PIN) == GPIO_PIN_SET) && !ButtonPressed)
     {
@@ -218,6 +393,8 @@ void UserInteractionTask(void const * argument)
 		taskYIELD();
   }
 }
+
+
 
 void ShowCommitedText()
 {
